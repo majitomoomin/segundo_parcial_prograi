181847b6ee4522c4370d6ed3243b8becf3ccd917
majitomoomin <mariajoseportill2@gmail.com>
2024-05-04 11:55:47 -0600

    commit #2

---
+++ Parcial-2/Parcial-2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_2
+{
+    internal class Empleados
+    {
+        public Empleados()
+        {
+            nombre = "";
+            tipo_empleado = "";
+            Salario = "";
+            horastrabajadas = "";
+        }
+        public string nombre;
+        public string tipo_empleado;
+        public string Salario;
+        public string horastrabajadas;
+       
+
+        }
+    }
